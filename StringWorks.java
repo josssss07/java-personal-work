4b91c7f25e6be64850d752e7a39e18092b25f95e
josssss07 <joshuadesai03@gmail.com>
2023-09-20 14:37:41 +0530

    prac2

--- StringWorks.java
+++ StringWorks.java
@@ -6,6 +6,9 @@ public class StringWorks {
         w.alternateToUpper(s);
         w.displayCenterWord(s);
         w.longestWord(s);
+        w.stringReverse(s);
+        w.alternateWordReverse(s);
+        w.alternateWordDelete(s, "very");
     }
 
     //q1: alternating words to uppercase
@@ -53,4 +56,39 @@ public class StringWorks {
         }
         System.out.println(maxstr);
     }
+
+    void stringReverse(String s){
+        StringBuffer sb = new StringBuffer(s);
+        sb.reverse();
+        System.out.println(sb);
+    }
+
+    void alternateWordReverse(String s){
+        String newString = "";
+        String[] sarr = s.split("\\s+");
+        for (int i=0; i< sarr.length; i++){
+            StringBuffer sb = new StringBuffer(sarr[i]);
+            if (i%2 ==0){
+                newString = newString+ sb.reverse()+" ";
+            }
+            else {
+                newString = newString + sarr[i]+ " ";
+            }
+        }
+        System.out.println(newString);
+
+    }
+
+    void alternateWordDelete(String s, String wordToDelete){
+        String[] sarr = s.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < sarr.length; i++) {
+            if (!sarr[i].equals(wordToDelete)) {
+                sb.append(sarr[i]).append(" ");
+            }
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+
 }
