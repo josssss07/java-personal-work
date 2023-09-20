a072c93d530bdd577c9226425aa8565b75c02a14
josssss07 <joshuadesai03@gmail.com>
2023-09-20 15:29:31 +0530

    prac2

--- StudentArrayList.java
+++ StudentArrayList.java
@@ -92,36 +92,37 @@ class Student{
 
 public class StudentArrayList {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Student> ar = new ArrayList<Student>();
+        try (Scanner sc = new Scanner(System.in)) {
+            ArrayList<Student> ar = new ArrayList<Student>();
 
-        System.out.println("Enter the number of records to be stored: ");
-        int records_no = sc.nextInt();
-        //System.out.println(records_no);
+            System.out.println("Enter the number of records to be stored: ");
+            int records_no = sc.nextInt();
+            //System.out.println(records_no);
 
-        for(int i=0; i<records_no; i++){
-            System.out.println("Record number: "+ ( i+1 ) ) ;
-            System.out.println("Enter UID: ");
-            int temp_uid = sc.nextInt();
-            System.out.println("Enter rno: ");
-            int temp_rno = sc.nextInt();
-            System.out.println("Enter age: ");
-            int temp_age = sc.nextInt();
-            System.out.println("Enter name: ");
-            String temp_name = sc.next();
-            System.out.println("Enter Course: ");
-            String temp_course = sc.next();
+            for(int i=0; i<records_no; i++){
+                System.out.println("Record number: "+ ( i+1 ) ) ;
+                System.out.println("Enter UID: ");
+                int temp_uid = sc.nextInt();
+                System.out.println("Enter rno: ");
+                int temp_rno = sc.nextInt();
+                System.out.println("Enter age: ");
+                int temp_age = sc.nextInt();
+                System.out.println("Enter name: ");
+                String temp_name = sc.next();
+                System.out.println("Enter Course: ");
+                String temp_course = sc.next();
 
 
-            //to put data into array list
-            Student st  = new Student(temp_uid, temp_rno, temp_age, temp_name, temp_course);
-            ar.add(st);
-        }
+                //to put data into array list
+                Student st  = new Student(temp_uid, temp_rno, temp_age, temp_name, temp_course);
+                ar.add(st);
+            }
 
-        //System.out.println("Data Insertion Completed.\n Would you like to view the data\nEnter Y for yes or N for no");
-        //String viewDataDecision = sc.next();
-        for(Student s : ar) {
-            System.out.println(s.getUid() + " " + s.getAge() + " " + s.getRno() + " " + s.getName() + " " + s.getCourse() + "\n");
+            //System.out.println("Data Insertion Completed.\n Would you like to view the data\nEnter Y for yes or N for no");
+            //String viewDataDecision = sc.next();
+            for(Student s : ar) {
+                System.out.println(s.getUid() + " " + s.getAge() + " " + s.getRno() + " " + s.getName() + " " + s.getCourse() + "\n");
+            }
         }
     }
 }
